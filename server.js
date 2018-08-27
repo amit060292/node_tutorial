d47052d2f35db1387fb6f29a326398e3586800b7
Amit Jain <amit.jain2@tothenew.com>
2018-08-27 11:50:56 +0530

    node tutorial setup

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const hbs = require('hbs');
+
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+	var now = new Date().toString();
+	var log = `${now} : ${req.method} ${req.path}`;
+	console.log(log);
+	next();
+});
+app.use((req, res, next) => {
+	res.render('maintainance.hbs', {
+		pageTitle: "We'l be back soon!!",
+	});
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+	res.render('home.hbs', {
+		pageTitle: 'Home Page',
+	});
+});
+
+app.get('/about', (req, res) => {
+	res.render('about.hbs', {
+		pageTitle: 'About Page',
+	});
+});
+
+app.get('/bad', (req, res) => {
+	res.send({
+		errorMessage: 'Unable to handle request',
+	});
+});
+
+app.listen(3000, () => {
+	console.log('server is up on port 3000');
+});
